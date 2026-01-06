d345c95707e25eef6b1e3427ba0237e574f5767f
Ank911007 <145314378+Ank911007@users.noreply.github.com>
2026-01-07 01:33:27 +0530

    Add k3s installation and configuration script
    Added installation commands for k3s and configuration setup.

--- k3s/install.sh
+++ k3s/install.sh
@@ -1,1 +1,12 @@
 # k3s/install.sh
+sudo apt update && sudo apt upgrade -y
+curl -sfL https://get.k3s.io | sh -
+mkdir -p $HOME/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+echo 'export KUBECONFIG=$HOME/.kube/config' >> ~/.bashrc
+source ~/.bashrc
+sudo systemctl status k3s
+kubectl get nodes
+kubectl get pods -A
