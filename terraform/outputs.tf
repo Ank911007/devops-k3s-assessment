e6ff00f7431c5337c6d40df7a898cb12f02b9233
Ank911007 <145314378+Ank911007@users.noreply.github.com>
2026-01-07 01:48:08 +0530

    Define outputs for instance IP and SSH command
    Add outputs for instance public IP and SSH command

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,8 @@
 # terraform/outputs.tf
+output "instance_public_ip" {
+  value = aws_instance.ubuntu_server.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i <your-key.pem> ubuntu@${aws_instance.ubuntu_server.public_ip}"
+}
