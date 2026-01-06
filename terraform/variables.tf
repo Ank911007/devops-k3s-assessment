efd8022566b5f1c331236b9149579294067b249e
Ank911007 <145314378+Ank911007@users.noreply.github.com>
2026-01-07 01:47:31 +0530

    Define AWS and Ubuntu variables in variables.tf
    Add variables for AWS region, key name, and Ubuntu AMI.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
 # terraform/variables.tf
+variable "aws_region" {
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
+variable "key_name" {
+  description = "Existing AWS EC2 key pair name"
+  type        = string
+}
+
+variable "ubuntu_ami" {
+  description = "Ubuntu 24.04 LTS AMI ID"
+  type        = string
+}
